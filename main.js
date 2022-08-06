39f01c816691cf4a1d2cf2767356caf6d483ec9a
pranjalsingla <88027633+pranjalsingla@users.noreply.github.com>
2022-08-06 07:50:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+function preload(){
+
+}
+
+function setup(){
+   canvas = createCanvas(300,300);
+   canvas.center();
+   video = createCapture(VIDEO);
+   video.size(300,300);
+   video.hide();
+   poseNet = ml5.poseNet(video,modelLoaded);
+   poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("POSENET IS INITIALIZED");
+}
+
+function draw(){
+    image(video,0,0,300,300);
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        console.log("left ear x = "+results[0].pose.leftEar.x);
+        console.log("left ear y = "+results[0].pose.leftEar.y);
+        console.log("right ear x = "+results[0].pose.rightEar.x);
+        console.log("right ear y = "+results[0].pose.rightEar.y);
+    }
+}
+
+function take_snapshot(){
+    save('my_filter_image.png')
+}
